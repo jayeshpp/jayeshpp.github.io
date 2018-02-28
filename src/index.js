7f6607341dd53de5ec0497ca821396b9408326b2
jayeshpp007@gmail.com <jayeshpp007@gmail.com>
2018-02-28 15:05:57 +0530

    profile

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <Homepage/>,
+    document.getElementById("root")
+);
+
+function Homepage() {
+    return (
+        <div>
+            Welcome to my Website!...
+        </div>
+    )
+}
