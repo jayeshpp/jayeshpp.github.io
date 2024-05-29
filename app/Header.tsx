0d00ad4a9878c9ed32489208adbf6d051b5bed8a
jayeshpp <jayeshpp007@gmail.com>
2024-05-29 11:21:28 +0100

    update to mobile first

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => (
   <header>
-    <div className="container m-auto h-20 flex items-center">
+    <div className="container m-auto px-6 md:px-20 h-20 flex items-center">
         <div className="logo">PJ <span>Studio</span></div>
     </div>
   </header>
--- components/about/About.tsx
+++ components/about/About.tsx
@@ -1,22 +1,35 @@
 "use client";
+import { sendGTMEvent } from '@next/third-parties/google'
 import Image from "next/image";
 import { Button } from "../button";
 
 export const About = () => {
   const onDownloadCV = () => {
+    sendGTMEvent({ event: 'downloadCVButtonClicked', value: 'downloadCV' })
     return window.open("/assets/pdf/jayeshp-cv.pdf");
   };
 
   const onSayHello = () => {
+    sendGTMEvent({ event: 'sayHelloButtonClicked', value: 'sayHello' })
     window.location.href = `mailto:jayesh.puthalathpoyil@gmail.com?subject=Hello! Jai&body=`;
   };
 
   return (
-    <section className="container m-auto flex items-center justify-between py-16">
+    <section className="container px-6 md:px-20 m-auto flex flex-col items-center justify-between py-16 md:flex-row-reverse">
       <div className="flex-1">
-        <h1 className="title-1 pb-8">Jayesh</h1>
-        <h2 className="title-2 pb-5">Frontend Engineer</h2>
-        <p className="text-xl max-w-[530px] pb-5">
+        <Image
+          src="/assets/images/jayesh.webp"
+          alt="Jayesh"
+          width={476}
+          height={476}
+          unoptimized
+          className="m-auto"
+        />
+      </div>
+      <div className="flex-1">
+        <h1 className="title-1 pb-4 md:pb-8">Jayesh</h1>
+        <h2 className="title-2 pb-3 md:pb-5">Frontend Engineer</h2>
+        <p className="text-base md:text-xl max-w-[530px] pb-5">
           I am a frontend-heavy fullstack developer with a proven track record
           of delivering pixel-perfect, high-quality, and scalable web
           applications.
@@ -27,16 +40,6 @@ export const About = () => {
         <Button variant="outlined" className="ml-2" onClick={onDownloadCV}>
           Download CV
         </Button>
-      </div>
-      <div className="flex-1">
-        <Image
-          src="/assets/images/jayesh.webp"
-          alt="Jayesh"
-          width={476}
-          height={476}
-          unoptimized
-          className="m-auto"
-        />
       </div>
     </section>
   );
